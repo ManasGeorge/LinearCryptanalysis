5c2e9e156bfee72435e87835b56d9e0f4e6c4c9a
Manas George <manas.george@gatech.edu>
2014-11-06 21:41:24 -0500

    Finished the class, added printf statements for clarity.

--- analyst.cpp
+++ analyst.cpp
@@ -1,20 +1,26 @@
 #include "analyst.h"
 #include <cstdlib>
+#include <cstdio>
 
-Analyst::Analyst(Cipher cipher) : c(cipher) {
+Analyst::Analyst(const Cipher &cipher) : c(cipher) {
 }
 
 int Analyst::analyze() {
     int key = 0;
     int score[maxinput];
+    printf("Approximating SBoxes\n");
 
     for (int i = 0 ; i < numBoxes; i++)
         approxSBox(i);
 
+    printf("Finding best SBox approximations\n");
+
     for (int i = 0; i < numBoxes; i++)
         findBestApprox(i);
 
+    printf("Chaining Approximations\n");
     chainApproximations();
+    printf("Guessing final key\n");
 
     for (int guess = 0; guess < maxinput; guess++) {
         score[guess] = 0;
@@ -25,6 +31,11 @@ int Analyst::analyze() {
         }
     }
 
+    for (int i = 0; i < maxinput; i++)
+        if (score[i] > score[key])
+            key = i;
+
+    printf("The guessed final key is: %d\n", key);
     return key;
 }
 
@@ -71,16 +82,42 @@ void Analyst::chainApproximations() {
 
     for (int i = 0; i < numBoxes; i++)
         for (int j = 0; j < maxinput; j++) {
-            int left = lefts[j];
-            if(left != -1) {
-                int right = bestApprox[i][left];
-                if(right == -1)
+            if (lefts[j] != -1) {
+                int right = bestApprox[i][lefts[j]];
+
+                if (right == -1)
                     lefts[j] = -1;
                 else
                     lefts[j]  = c.perms(i, right);
             }
         }
 
-    for(int i = 0; i < maxinput; i++)
+    for (int i = 0; i < maxinput; i++)
         totalApprox[i] = lefts[i];
+}
+
+int Analyst::countParityMatch(int plain, int decrypt) {
+    int ret = 0;
+
+    for (int left  = 0; left  < maxinput; left ++) {
+        int right = totalApprox[left];
+
+        if (right != -1)
+            if (parity(left, plain) == parity(right, decrypt))
+                ret++;
+    }
+
+    return ret;
+}
+
+void Analyst::findBestApprox(int i) {
+    for (int left = 0; left < maxinput; left++) {
+        int max = 0;
+
+        for (int right = 0; right < maxinput; right++)
+            if (bias[i][left][right] > max)
+                max = bias[i][left][right];
+
+        bestApprox[i][left] = (max == 0) ? (-1) : (max);
+    }
 }
